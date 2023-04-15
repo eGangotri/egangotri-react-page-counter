4ff127a5c827b5d1b855cade899b742c7bb4c322
egangotri <egangotritrust@gmail.com>
2023-04-15 08:43:44 +0530

    added public

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import './index.css';
 
+import PageCounter from 'pages/PageCounter';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import PageCounter from 'pages/PageCounter';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
